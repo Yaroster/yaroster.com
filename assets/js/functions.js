13e8dd94b96441001ee4bada067585a667a01b77
Yaroster <39062152+Yaroster@users.noreply.github.com>
2021-02-03 17:06:49 +0400

    weebanalyser

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -277,4 +277,23 @@ $( document ).ready(function() {
   workSlider();
   transitionLabels();
 
+  var search = document.getElementById('search');
+  var button = document.getElementById('button');
+  var input = document.getElementById('input');
+
+  function loading() {
+    search.classList.add('loading');
+
+    setTimeout(function() {
+      search.classList.remove('loading');
+    }, 1500);
+  }
+
+  button.addEventListener('click', loading);
+
+  input.addEventListener('keydown', function() {
+    if(event.keyCode == 13) loading();
+  });
+
+
 });
